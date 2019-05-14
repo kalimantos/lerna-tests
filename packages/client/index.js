f92d944c3a47ee251525ed8a2ab1c9bcda920a2d
Matteo Dellea <matteo.dellea@mikamai.com>
2019-05-14 12:13:28 +0200

    cose

---
+++ packages/client/index.js
@@ -0,0 +1,6 @@
+const formatters = require('formatters');
+
+module.exports = (function() {
+  console.log('CLIENT!');
+  return "CLIENT" + formatters();
+})();
--- packages/translations/lib/translations.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = translations;
-
-function translations() {
-    // TODO
-}
